822f59aa3077a39364d66c9533bbfc33916c3696
Samuel Darkwah Manu <manusam14@yahoo.com>
2021-07-20 22:31:35 +0000

    Complete product detail page

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Product Detail Page</h1>
+  <div id="page-wrap">
+    <div id="img-wrap">
+      <img :src="product.imageUrl" alt="">
+    </div>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">£{{ product.price }}</h3>
+      <p>Average Rating: {{ product.averageRating}}</p>
+      <button id="add-to-cart">Add to Cart</button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { products } from "../fake-data";
+
 export default {
-    name: 'ProductDetailPage'
+    name: 'ProductDetailPage',
+    data() {
+      return { product: products.find((p) => p.id === this.$route.params.id) }
+    }
 }
 </script>
 
