e11b4b60bf0f0d128310f53215f2bc96f3cca5b6
Samuel Darkwah Manu <manusam14@yahoo.com>
2021-07-20 21:45:05 +0000

    Style views and complete products page

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,13 +1,70 @@
 <template>
-  <h1>Products Page</h1>
+  <div id="page-wrap">
+    <div class="grid-wrap">
+      <div 
+      v-for="product in products"
+      class="product-item"
+      :key="product.id">
+      <img :src="product.imageUrl" :alt='"Picture of " + product.name'>
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">£{{ product.price }}</p>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
+
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import { products } from "../fake-data";
+
 export default {
-    name: 'ProductsPage'
+    name: 'ProductsPage',
+    data() {
+      return {
+        products
+      }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+  .grid-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 16px;
+  }
 
+  .product-item {
+    align-items: center;
+    border-radius: 8px;
+    box-shadow: 0px 2px 5px #888;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2%;
+    padding: 20px;
+    position: relative;
+    width: 32%;
+  }
+
+  .product-name {
+    margin-bottom: 0;
+  }
+
+  img {
+    height: 200px;
+    width: 200px;
+  }
+
+  a {
+    width: 100%;
+  }
+
+  button {
+    width: 100%;
+  }
 </style>
