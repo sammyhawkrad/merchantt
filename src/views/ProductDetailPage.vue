80d1bfdb0d7e1abb7846fdbf98a725580ac53254
Samuel Darkwah Manu <manusam14@yahoo.com>
2021-08-04 03:12:04 +0000

    Make add to cart button functional

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -7,7 +7,7 @@
       <h1>{{ product.name }}</h1>
       <h3 id="price">£{{ product.price }}</h3>
       <p>Average Rating: {{ product.averageRating}}</p>
-      <button id="add-to-cart">Add to Cart</button>
+      <button id="add-to-cart" @click="addToCart">Add to Cart</button>
       <h4>Description</h4>
       <p>{{ product.description }}</p>
     </div>
@@ -29,6 +29,13 @@ export default {
       const result = await axios.get(`/api/products/${this.$route.params.id}`);
       const product = result.data;
       this.product = product;
+    },
+    methods: {
+      async addToCart() {
+        await axios.post('/api/users/12345/cart', {
+          productId: this.$route.params.id
+        });
+      }
     }
 }
 </script>
